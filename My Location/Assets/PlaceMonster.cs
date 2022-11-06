fd4608f809111414c8a90073264034ef0a26301f
Scrim3226 <96692917+Scrim3226@users.noreply.github.com>
2022-11-06 17:35:30 +0300

    Upload Finish Progect
    ...

---
+++ My Location/Assets/PlaceMonster.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+
+public class PlaceMonster : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    private GameObject monster;
+
+    private bool CanPlaceMonster()
+    {
+        return monster == null;
+    }
+
+
+    void OnMouseUp()
+    {
+
+        if (CanPlaceMonster())
+        {
+
+            monster = (GameObject)
+              Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+
+        }
+        else if (CanUpgradeMonster())
+        {
+            monster.GetComponent<MonsterData>().IncreaseLevel();
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+            // TODO: вычитать золото
+        }
+    }
+
+    private bool CanUpgradeMonster()
+    {
+        if (monster != null)
+        {
+            MonsterData monsterData = monster.GetComponent<MonsterData>();
+            MonsterLevel nextLevel = monsterData.GetNextLevel();
+            if (nextLevel != null)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
